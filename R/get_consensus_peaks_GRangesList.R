a5cd002aa794d96e2daeb399067a08bcc6bb877c
eggrandio <eggrandio@gmail.com>
2022-04-19 09:42:29 +0200

    Added commented section for BRC1 direct targets

--- R/get_consensus_peaks_GRangesList.R
+++ R/get_consensus_peaks_GRangesList.R
@@ -4,7 +4,8 @@ get_consensus_peaks_GRangesList = function(granges_list,
   
   peak_coverage = GenomicRanges::GRangesList(granges_list) %>% GenomicRanges::coverage()
   
-  covered_ranges = IRanges::slice(peak_coverage, lower=rep_number, rangesOnly=T) %>% GenomicRanges::GRanges() %>% 
+  covered_ranges = IRanges::slice(peak_coverage, lower=rep_number, rangesOnly=T) %>% 
+    GenomicRanges::GRanges() %>% 
     plyranges::mutate(peak_name = paste0("peak_",rep(1:length(.)))) #%>% 
   #plyranges::mutate(name = gene_id) %>% 
   #plyranges::mutate(type = rep("gene",length(.)))
